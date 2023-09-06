f7b2a4b8692ed8647d94ee83ba66effcb7f5410e
Amit Ganguly <amitgangulyin@gmail.com>
2023-09-06 12:24:14 -0400

    Create Python_Operator.py

---
+++ Python_Operator.py
@@ -0,0 +1,20 @@
+# Numercal operatos in Python
+
+# + for addition
+# - for substractions 
+# * for multiplication
+# / for float division
+#// for integer division
+# ** for power calculation
+# % for modulus 
+
+x = 5
+y = 3 
+
+print("Addition of x + y = ", x + y)
+print("Substraction of x - y = ", x - y)
+print("Multipliccation of x * y = ", x * y)
+print("float Division of x / y = ", x / y)
+print("integer division of x // y = ", x // y)
+print("Modulus of x % y = ", x % y)
+print("Power of y on x = ", x ** y)
