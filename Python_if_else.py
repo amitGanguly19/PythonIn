46f97ce800c40ae0e01971cb39a51b742611718e
Amit Ganguly <amitgangulyin@gmail.com>
2023-09-07 13:42:26 -0400

    Create Python_if_else.py

---
+++ Python_if_else.py
@@ -0,0 +1,24 @@
+# How to use If-else in Python
+
+x = 10
+y = 5
+
+if x == y:
+    print("Yes, X is Equals to Y")
+else:
+    print("No, X is not Equals to Y")    
+
+
+# Is it mandatory to use else block with if?
+
+#a = 50
+#if a == 50:
+    #print("Yes, A is Equals to  50 !!")
+#print("bye !!!")
+
+a = 40
+
+if a == 50:
+    print("Yes, A is Equals to  50 !!")
+print("bye !!!")
+
